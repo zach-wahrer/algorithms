9bd3fa6f08a1735c99c57b3f327588fbd1ee79e3
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-23 13:37:47 -0700

    Move Stack class into models folder

--- 2-basic-data-structures/stack/reverse_string.py
+++ 2-basic-data-structures/stack/reverse_string.py
@@ -1,18 +1,4 @@
-class Stack():
-    def __init__(self):
-        self.stack = []
-
-    def add(self, value):
-        self.stack.append(value)
-
-    def pop(self):
-        return self.stack.pop()
-
-    def is_empty(self):
-        return self.stack == []
-
-    def peek(self):
-        return self.stack[-1]
+from models.stack import Stack
 
 
 def rev_string(string):
