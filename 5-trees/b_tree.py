d58f83b9f976c52a3db8c04089e63949c48b41d3
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-27 16:00:24 -0700

    Add inorder traversal to b_tree

--- 5-trees/b_tree.py
+++ 5-trees/b_tree.py
@@ -45,9 +45,19 @@ def postorder(tree):
         print(tree.get_root_val())
 
 
+def inorder(tree):
+    if tree:
+        inorder(tree.get_left_child())
+        print(tree.get_root_val())
+        inorder(tree.get_right_child())
+
+
 print("Preorder: ")
 preorder(tree)
 print("\n")
 print("Postorder: ")
 postorder(tree)
+print("\n")
+print("Inorder: ")
+inorder(tree)
 print("\n")
